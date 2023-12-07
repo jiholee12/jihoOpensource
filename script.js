9ce5e6939bb041ec8e4576316eb10238ac089e9e
Lee ji ho <xhdajfr@naver.com>
2023-12-08 02:04:21 +0900

    스크립(18)

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-function buttonClick(value){
-    if(isNaN(value)){
-        handleSymbol(value);
-    }else{
-        handleNumber(value);
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+            buffer = '0';
+            runninTotal = 0;
+            break;
+
+            case '=':
+                if(previousOperator === null){
+                    return
+                }
+                flushOperation(parseInt(buffer));
+                previousOperator = null;
+                buffer = runninTotal;
+                runninTotal = 0;
+                break;
+
+                case'←':
+                if(buffer.length ===1){
+                    buffer ='0';
+                }else{
+                    buffer = buffer.substring(0,buffer.length -1);
+                }
+                break; 
+
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    handleMath(symbol);
+                    break;
     }
-    screen.innerText = buffer;
 }
