3967a5d866b25368c7f77d09ae9fac2acd2cfc4d
Lee ji ho <xhdajfr@naver.com>
2023-12-08 02:06:03 +0900

    스크립(20)

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-function handleMath(symbol){
-    if(buffer ==='0'){
-        return;
-    }
-
-    const intBuffer = parseInt(buffer);
-
-    if(runninTotal === 0){
-        runninTotal = intBuffer;
-    }else{
-        flushOperation(intBuffer);
-    }
-    previousOperator = symbol;
-    buffer = '0'
+function init(){
+    document.querySelector(' .calc-buttons').addEventListener('click',function(event){
+        buttonClick(event.target.innerText);
+    })
 }
 
-function flushOperation(intBuffer){
-    if(previousOperator === '+'){
-        runninTotal += intBuffer;
-    }else if(previousOperator === '-'){
-        runninTotal -= intBuffer;
-    }else if(previousOperator === '*'){
-        runninTotal *= intBuffer
-    }else if(previousOperator === '/'){
-        runninTotal /= intBuffer;
-    }
-}
-
-function handleNumber(numberString){
-    if(buffer === '0'){
-        buffer = numberString;
-    }else{
-        buffer += numberString;
-    }
-}
+init();
